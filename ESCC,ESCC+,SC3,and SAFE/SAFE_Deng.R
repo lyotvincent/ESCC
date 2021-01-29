7ce999e1ddbc57401241a8774a4cd2e34c5dd8be
lyotvincent <jianliu@hit.edu.cn>
2021-01-29 20:30:26 +0800

    first commit

---
+++ ESCC,ESCC+,SC3,and SAFE/SAFE_Deng.R
@@ -0,0 +1,21 @@
+for( tot in 1:100)
+{
+  library("SAFEclustering")
+  library('SC3')
+  library(SingleCellExperiment)
+  library('tidyverse')
+  a<-read_rds('deng-rpkms.rds')
+  print(a)
+  data<-assay(a, "logcounts")
+  counts(a) <-assay(a, "normcounts")
+  library(SC3)
+  deng <- sc3_prepare(a)
+  gene_filter <- rowData(deng)$sc3_gene_filter
+  matrix_filtered=exp(data[which(gene_filter),1:268])
+  cluster.results <- individual_clustering(inputTags = matrix_filtered, mt_filter = FALSE, nGene_filter = FALSE, SC3 = TRUE, gene_filter = FALSE, CIDR = TRUE, nPC.cidr = NULL, Seurat = TRUE, nPC.seurat = NULL, resolution = 0.9, tSNE = TRUE, dimensions = 3, perplexity = 30, SEED = 123)
+  cluster.ensemble <- SAFE(cluster_results = cluster.results, program.dir = "gpmetis_and_shmetis_for_Linux", k_min=10,k_max=10,
+                           MCLA = TRUE, CSPA = TRUE, HGPA = TRUE, SEED = 123)
+  library(cidr)
+  cat(cluster.ensemble$optimal_clustering,file="SAFE_Deng.txt",append=TRUE)
+  cat("\n",file="SAFE_Deng.txt",append=TRUE)
+}
---
+++ ESCC,ESCC+,SC3,and SAFE/SAFE_Yan.R
@@ -0,0 +1,21 @@
+for( tot in 1:27)
+{
+  library("SAFEclustering")
+  library('SC3')
+  library(SingleCellExperiment)
+  library('tidyverse')
+  a<-read_rds('yan.rds')
+  print(a)
+  data<-assay(a, "logcounts")
+  counts(a) <-assay(a, "normcounts")
+  library(SC3)
+  deng <- sc3_prepare(a)
+  gene_filter <- rowData(deng)$sc3_gene_filter
+  matrix_filtered=exp(data[which(gene_filter),1:90])
+  cluster.results <- individual_clustering(inputTags = matrix_filtered, mt_filter = FALSE, nGene_filter = FALSE, SC3 = TRUE, gene_filter = FALSE, CIDR = TRUE, nPC.cidr = NULL, Seurat = TRUE, nPC.seurat = NULL, resolution = 0.9, tSNE = TRUE, dimensions = 3, perplexity = 30, SEED = 123)
+  cluster.ensemble <- SAFE(cluster_results = cluster.results, program.dir = "gpmetis_and_shmetis_for_Linux", k_min=7,k_max=7,
+                           MCLA = TRUE, CSPA = TRUE, HGPA = TRUE, SEED = 123)
+  library(cidr)
+  cat(cluster.ensemble$optimal_clustering,file="SAFE_Yan.txt",append=TRUE)
+  cat("\n",file="SAFE_Yan.txt",append=TRUE)
+}
